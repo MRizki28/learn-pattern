9eb86fbb3eba119956d350ad463147df4fd94e1c
rizki <muhammadrizkitkj123@gmail.com>
2023-08-12 10:47:13 +0800

    create hanlde upsert data

--- app/ApiResponse/MahasiswaResponse.php
+++ app/ApiResponse/MahasiswaResponse.php
@@ -3,6 +3,7 @@
 namespace App\ApiResponse;
 
 use App\Repositories\MahasiswaRepository;
+use Illuminate\Http\Request;
 
 class MahasiswaResponse
 {
@@ -34,6 +35,30 @@ class MahasiswaResponse
                 'code' => 400,
                 'message' => 'failed',
                 'errors' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $data = $this->MahasiswaRepository->createData($request);
+        if ($data instanceof \Illuminate\Validation\Validator && $data->fails()) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Check your validation',
+                'errors' => $data->errors()
+            ]);
+        } elseif ($data instanceof \Throwable) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Failed',
+                'errors' => $data->getMessage()
+            ]);
+        } else {
+            return response()->json([
+                'code' => 201,
+                'message' => 'Success create data',
+                'data' => $data
             ]);
         }
     }
