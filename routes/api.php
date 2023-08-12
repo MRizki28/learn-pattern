3b6f23a538718aabddf5370566b2dec4a3bbf9cd
rizki <muhammadrizkitkj123@gmail.com>
2023-08-12 10:48:26 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', [MahasiswaController::class, 'getAllData']);
+Route::post('/create', [MahasiswaController::class, 'createData']);
