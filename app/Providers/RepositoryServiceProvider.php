81aeb276bb0e02069b2b2d82530fe2e5f703d9db
rizki <muhammadrizkitkj123@gmail.com>
2023-08-12 10:56:34 +0800

    update

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\MahasiswaInterfaces;
+use App\Repositories\MahasiswaRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(MahasiswaInterfaces::class, MahasiswaRepository::class);
     }
 
     /**
