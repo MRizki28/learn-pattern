30125015e733289db88a7db969a2176048994ab3
rizki <muhammadrizkitkj123@gmail.com>
2023-08-13 18:41:03 +0800

    update

--- database/migrations/2023_08_11_143753_create_tb_mahasiswa_table.php
+++ database/migrations/2023_08_11_143753_create_tb_mahasiswa_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tb_mahasiswa', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
+            $table->boolean('entry_count')->default(false);
             $table->timestamps();
         });
     }
